606ec8889ab9101b4a1a2ac9fe0d688755343f38
William Lowrimore <wlowrimore@gmail.com>
2023-02-22 18:33:43 -0600

    initial setup with learning resource component

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <LearningResource
+    v-for="res in storedResources"
+    :key="res.id"
+    :title="res.title"
+    :description="res.description"
+    :link="res.link"
+  />
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue'
+
+export default {
+  components: {
+    LearningResource
+  },
+  data () {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google...',
+          link: 'https://google.org'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li>
+      <div>
+        <header>
+          <h3>{{ title }}</h3>
+          <button>Delete</button>
+        </header>
+      </div>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link']
+}
+</script>
