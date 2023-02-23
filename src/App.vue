780a294fdc553b808faaedab415f9c89c3bd4dfd
William Lowrimore <wlowrimore@gmail.com>
2023-02-22 19:48:00 -0600

    breaks up resource components, adds styling, and adds BaseCard Component and HeaderComponent with new file structuring.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <LearningResource
-    v-for="res in storedResources"
-    :key="res.id"
-    :title="res.title"
-    :description="res.description"
-    :link="res.link"
-  />
+  <TheHeader title="RememberMe" />
+  <StoredResources :resources="storedResources" />
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue'
+import StoredResources from './components/learning-resources/StoredResources.vue'
+import TheHeader from './components/layouts/TheHeader.vue'
 
 export default {
   components: {
-    LearningResource
+    StoredResources,
+    TheHeader
   },
   data () {
     return {
@@ -35,3 +32,19 @@ export default {
   }
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
